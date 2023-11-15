b4e297944efc3949b3e7ff4ca84fd88644aad626
= <=>
2023-11-15 14:35:54 +0100

    fixed padding for small screens

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,6 +1,9 @@
 function Button({ text, type, buttonIcon = '', clickAction }) {
   return (
-    <button className={`btn btn-${type} mt-4 flex gap-2`} onClick={clickAction}>
+    <button
+      className={`btn btn-${type} mt-4 flex gap-2 w-100`}
+      onClick={clickAction}
+    >
       <span className="button-text">{text}</span>
       <span className="button-icon">{buttonIcon}</span>
     </button>
